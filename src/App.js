f2ba6e9db2f5e0ecafc8e218800a7beadaa38246
AKASH <ar0938344@gmail.com>
2021-06-10 04:47:54 +0530

    created board and square components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Board from './components/Board'
+
 const App = () => {
   return(
-    <>
-    <h1>Welcome to the React Parcel Micro App! </h1>
-    <p>Hard to get more minimal than this React app. </p>
-    <p> APP </p>
-  </>
+    <div>
+    <h1>TIC TAC TOE</h1>
+   <Board/>
+  </div>
 );
   
 }
