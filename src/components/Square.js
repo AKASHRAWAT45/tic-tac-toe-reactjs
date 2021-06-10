d1cfe31c8c455095b0b424dcb3217af03c8d2794
AKASH <ar0938344@gmail.com>
2021-06-11 04:36:58 +0530

    added board state and also react events

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Square = ({value}) => {
+const Square = ({value , onClick }) => {
 
   return (
-<button type = "button" className ="square" >{value}</button>
-  )
-}
+<button type = "button" className ="square"  onClick = {onClick}>
+  {value}
+  </button>
+  );
+};
  
-export default Square
+export default Square;
 
