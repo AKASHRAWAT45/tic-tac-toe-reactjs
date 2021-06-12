9ec3e17818d985f78ab05bc4664688bc90d1fc99
AKASH <ar0938344@gmail.com>
2021-06-13 05:06:40 +0530

    highlight winning squares and added start new game button

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,35 +1,38 @@
-import React  from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = ({ board , handleSquareClick}) => 
-{
-const renderSquare = (position)=>{
+const Board = ({ board, handleSquareClick, winningSquares }) => {
+  const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
+
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+      />
+    );
+  };
+
   return (
-  < Square value = {board[position]}
-   onClick={() => handleSquareClick(position)}
-   />
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
   );
 };
 
-  return (
-<div className = "board">
-<div className = "board-row">
-  {renderSquare(0)}
-  {renderSquare(1)}
-  {renderSquare(2)}
-</div>
-
-<div className = "board-row">
-{renderSquare(3)}
-{renderSquare(4)}
-{renderSquare(5)}
-</div>
-<div className = "board-row">
-{renderSquare(6)}
-{renderSquare(7)}
-{renderSquare(8)}
-</div>
-</div>
-  );
-};
 export default Board;
