a95110e44f081330c1249d72ee0a4ee4d0654066
AKASH <ar0938344@gmail.com>
2021-06-12 22:26:26 +0530

    transform state to game history

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React , { useState } from "react";
-import Board from './components/Board'
-import { calculateWinner } from "./helpers";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
 const App = () => {
-  const [ board, setBoard ]= useState(Array(9).fill(null));
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const [ isXNext , setIsXNext] = useState(false);
+  const current = history[currentMove];
+
   
-  const winner = calculateWinner(board);
+
+  const winner = calculateWinner(current.board);
   const message = winner
-  ? `Winner is ${winner}`
-  : `Next player is ${isXNext ? 'X' : 'O'}`;
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
-  
-  const handleSquareClick = (position) =>{
-    if(board[position] || winner)
-    { return;}
-  
-   setBoard((prev) =>{
-     return prev.map((square , pos) =>{
-       if(pos === position)
-       {return isXNext ? 'X' : '0';}
-     return square;
-     });
-   }
-   );
-   setIsXNext((prev) =>!prev);
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
+    }
+
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove(prev => prev + 1);
   };
-  
 
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
 
-  return(
-    <div className = "app">
-    <h1>TIC TAC TOE</h1>
-    <h2>  {message}   </h2>
-   <Board board = {board} handleSquareClick = {handleSquareClick}/>
-  </div>
-);
-  
-}
 export default App;
-  
